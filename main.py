6bdbe79fe296597687dc27499ba248b03993faa7
Vivek Tiwari <vt2174781@gmail.com>
2021-04-17 08:33:34 +0530

    Day 1 Basics of Python ( 17-04-2021 )

---
+++ main.py
@@ -0,0 +1,26 @@
+# Python Single Line comment
+"""
+Python
+MultiLine
+Comment
+"""
+
+
+# Printing a single line
+print("Welcome To StepIn : - >  Vivek Tiwari")
+
+# Simple Mathematical Operation
+print(123 + 123)    # Addition
+print(123 - 123)    # Subtraction
+print(123 * 123)    # Multiplication
+print(123 / 123)    # Float Division
+print(123 % 123)    # Remainder
+print(123 // 123)   # Integer Division
+print(2 ** 3)       # POWER(EXPONENTIAL)
+
+# Find the Quotient and Remainder if 254 divided by 5
+Quotient = 254 // 5      # Here Quotient is a variable which store the integer value after result
+Remainder = 254 % 5      # Here Remainder is a variable which store integer as a remainder
+
+
+
